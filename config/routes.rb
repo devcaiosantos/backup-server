855c06d93f5b4f3fb1c0c105a498a04f17d6c7e4
Caio Luiz Dos Santos <caio21122001@gmail.com>
2023-09-30 19:00:47 -0300

    Criação Rota delete file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     post 'upload', to: 'uploads#upload'
     get 'list', to: 'uploads#list_backups'
     delete 'delete_backup', to: 'uploads#delete_backup'
+    delete 'delete_file', to: 'uploads#delete_file'
   end
 end
