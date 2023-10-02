5f42c0620c2ad5d458b4e3ab7b94aeecca1f72f3
Caio Luiz Dos Santos <caio21122001@gmail.com>
2023-10-01 21:59:01 -0300

    Alteração de método rotas de delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   namespace :api do
     post 'upload', to: 'uploads#upload'
     get 'download_backup', to: 'uploads#download'
-    delete 'delete_backup', to: 'uploads#delete'
+    post 'delete_backup', to: 'uploads#delete'
     
     get 'download_file', to: 'files#download'
-    delete 'delete_file', to: 'files#delete'
+    post 'delete_file', to: 'files#delete'
   end
   
   get 'backup', to: 'api/uploads#list'
